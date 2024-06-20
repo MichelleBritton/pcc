2c4fc534b90cb09ffa20713b8d8e593a0435c076
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-06-20 12:54:08 +0100

    Create put method

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -17,9 +18,10 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
     def get_object(self, pk):
         try:
-            # Attempt to retreive profile by primary key and return it
+            # Attempt to retrieve profile by primary key and return it
             profile = Profile.objects.get(pk=pk)
             return profile
         except Profile.DoesNotExist:
@@ -29,3 +31,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
