119037e1105675af008a583455cfc5ab36457f38
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-06-20 12:34:49 +0100

    Add view and urls

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -9,15 +9,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
 
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()        
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
+   
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'first_name',
-            'last_name', 'image', 'is_owner',
+            'last_name', 'image', 
         ]
