3b6498c663ca6b5f2c5f448551d619c61fc6fdf6
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-06-20 13:15:49 +0100

    Create custom permission for IsOwnerorReadOnly and update view and serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -9,10 +9,15 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField() 
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
    
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'first_name',
-            'last_name', 'image', 
+            'last_name', 'image', 'is_owner',
         ]
